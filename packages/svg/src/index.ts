60dd2d09c7ff934ce1a5ef2f6d644f8e54d8aaa5
Shukant Pal <sukantk2002@outlook.com>
2020-10-04 19:24:58 -0400

    Export full Paint, SVG-nodes API

--- packages/svg/src/index.ts
+++ packages/svg/src/index.ts
@@ -1,6 +1,15 @@
+export * from './paint/InheritedPaintProvider';
+export * from './paint/Paint';
+export * from './paint/PaintProvider';
+export * from './paint/PaintServer';
+
 export * from './SVGGraphicsGeometry';
 export * from './SVGGraphicsNode';
 export * from './SVGImageNode';
 export * from './SVGPathNode';
 export * from './SVGScene';
+export * from './SVGUseNode';
+export * from './SVGTextEngine';
+export * from './SVGTextEngineImpl';
+export * from './SVGTextNode';
 export * from './SVGUseNode';
