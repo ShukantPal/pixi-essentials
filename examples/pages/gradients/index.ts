a11f0430f1223977a5a645bc57fadd0df3724283
Shukant Pal <sukantk2002@outlook.com>
2024-04-28 22:58:53 -0700

    Upgrade @pixi-essentials/svg to v8
    - [x] Upgrade pnpm, Rush
    - [x] Upgrade @pixi-essentials/gradients to v8
    - [ ] Upgrade @pixi-essentials/texture-allocator to v8
    - [ ] Use built-in `Culler` instead of @pixi-essentials/cull
    - [ ] Upgrade @pixi-essentials/svg

---
+++ examples/pages/gradients/index.ts
@@ -0,0 +1,41 @@
+import {Application, RenderTexture, Sprite} from "pixi.js";
+import {GradientFactory} from "@pixi-essentials/gradients";
+
+
+document.addEventListener('DOMContentLoaded', async () => {
+    const app = new Application();
+    Object.assign(window, { app });
+
+    await app.init({
+        autoStart: false,
+        height: 600,
+        view: document.getElementById('view') as HTMLCanvasElement,
+        width: 800,
+    });
+
+    const gradient1 = GradientFactory.createLinearGradient(
+        app.renderer,
+        RenderTexture.create({ width: 800, height: 600 }),
+        {
+            x0: 0,
+            y0: 0,
+            x1: 800,
+            y1: 600,
+            colorStops: [
+                { color: 0xFF0000, offset: 0 },
+                { color: 0x00FF00, offset: 0.5 },
+                { color: 0x0000FF, offset: 1 },
+            ],
+        },
+    );
+    const gradient1Sprite = new Sprite(gradient1);
+
+    app.stage.addChild(gradient1Sprite);
+
+    requestAnimationFrame(() => {
+        app.render()
+    })
+
+    console.log('hERER')
+});
+
---
+++ examples/rollup.config.js
@@ -0,0 +1,21 @@
+const commonjs = require('@rollup/plugin-commonjs');
+const { nodeResolve } = require('@rollup/plugin-node-resolve');
+const sucrase = require('@rollup/plugin-sucrase');
+
+module.exports =
+  {
+    input: './pages/gradients/index.ts',
+    output: {
+      dir: './public/bundles/gradients',
+      format: 'es'
+    },
+    plugins: [
+      nodeResolve(),
+      sucrase({
+        include: ["**/*.ts"],
+        transforms: ['typescript']
+      }),
+      commonjs({ }),
+    ],
+    treeshake: false,
+  }
